1e9e1072fdbf510a4a3e6605f809ae7267987ea1
joterrance <gorka.zgz@gmail.com>
2024-10-23 15:29:13 +0200

    added git blame plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -136,6 +136,17 @@ require("lazy").setup({
   'tamago324/cmp-zsh',
   'norcalli/nvim-colorizer.lua',
   'lewis6991/gitsigns.nvim',
+  {
+    "f-person/git-blame.nvim",
+    -- load the plugin at startup
+    event = "VeryLazy",
+    opts = {
+        enabled = true,  -- if you want to enable the plugin
+        message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+        date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+        virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
+    },
+},
   'kdheepak/lazygit.nvim',
   'echasnovski/mini.nvim',
   {"simrat39/symbols-outline.nvim", config = function() require("symbols-outline").setup() end, enabled = not vscode },
