b9318e58cf4516db1ca1141b582b91b71ccebfa0
zowi <xxx@example.com>
2026-05-20 13:03:33 +0200

    Final structure refactor

---
+++ lua/git/gitsigns-config/init.lua
@@ -0,0 +1,10 @@
+require('gitsigns').setup {
+  current_line_blame = false,
+  signs = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+  },
+}
---
+++ lua/tools/live-preview-config/init.lua
@@ -0,0 +1,9 @@
+require("live-command").setup {
+  enable_highlighting = true,
+  inline_highlighting = true,
+  hl_groups = {
+    insertion = "DiffAdd",
+    deletion = "DiffDelete",
+    change = "DiffChange",
+  },
+}
