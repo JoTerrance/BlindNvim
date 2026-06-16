824102c6c612df0cb51fdedf0268683d4032af2b
JoTerrance <gorka.zgz@gmail.com>
2026-06-16 13:11:50 +0200

    refactor duplicate tools

--- lua/core/plugins/init.lua
+++ lua/core/plugins/init.lua
@@ -279,10 +279,6 @@ require("lazy").setup({
   {
     'ThePrimeagen/git-worktree.nvim',
     dependencies = { 'nvim-telescope/telescope.nvim' },
-    keys = {
-      { '<leader>gw', function() require('telescope').extensions.git_worktree.git_worktrees() end, desc = 'Git worktrees', mode = 'n' },
-      { '<leader>gW', function() require('telescope').extensions.git_worktree.create_git_worktree() end, desc = 'Create git worktree', mode = 'n' },
-    },
     config = function()
       require('git-worktree').setup()
       require('telescope').load_extension('git_worktree')
