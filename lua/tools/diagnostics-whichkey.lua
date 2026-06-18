69e211242c206e351e4634affd1ac1430a04624a
JoTerrance <gorka.zgz@gmail.com>
2026-06-18 13:01:25 +0200

    fix diagnostics menu

--- lua/tools/diagnostics-whichkey.lua
+++ lua/tools/diagnostics-whichkey.lua
@@ -35,19 +35,19 @@ local function register_which_key(desc_prefix)
   end
 
   wk.add({
-    { '<leader>uI', group = desc_prefix },
-    { '<leader>uIe', '<cmd>TinyInlineDiag enable<cr>', desc = desc_prefix .. ' enable inline diagnostics' },
-    { '<leader>uId', '<cmd>TinyInlineDiag disable<cr>', desc = desc_prefix .. ' disable inline diagnostics' },
-    { '<leader>uIt', '<cmd>TinyInlineDiag toggle<cr>', desc = desc_prefix .. ' toggle inline diagnostics' },
-    { '<leader>uIc', '<cmd>TinyInlineDiag toggle_cursor_only<cr>', desc = desc_prefix .. ' toggle cursor-only diagnostics' },
-    { '<leader>uIr', '<cmd>TinyInlineDiag reset<cr>', desc = desc_prefix .. ' reset inline diagnostics' },
-    { '<leader>uIF', group = 'Diagflow' },
-    { '<leader>uIFe', diagflow_action('enable'), desc = 'Enable floating diagnostics' },
-    { '<leader>uIFd', diagflow_action('disable'), desc = 'Disable floating diagnostics' },
-    { '<leader>uIFt', diagflow_action('toggle'), desc = 'Toggle floating diagnostics' },
-    { '<leader>uIFc', diagflow_action('set_scope', 'cursor'), desc = 'Cursor scope' },
-    { '<leader>uIFl', diagflow_action('set_scope', 'line'), desc = 'Line scope' },
-    { '<leader>uIFs', diagflow_action('toggle_scope'), desc = 'Toggle scope' },
+    { '<leader>tI', group = desc_prefix },
+    { '<leader>tIe', '<cmd>TinyInlineDiag enable<cr>', desc = desc_prefix .. ' enable inline diagnostics' },
+    { '<leader>tId', '<cmd>TinyInlineDiag disable<cr>', desc = desc_prefix .. ' disable inline diagnostics' },
+    { '<leader>tIt', '<cmd>TinyInlineDiag toggle<cr>', desc = desc_prefix .. ' toggle inline diagnostics' },
+    { '<leader>tIc', '<cmd>TinyInlineDiag toggle_cursor_only<cr>', desc = desc_prefix .. ' toggle cursor-only diagnostics' },
+    { '<leader>tIr', '<cmd>TinyInlineDiag reset<cr>', desc = desc_prefix .. ' reset inline diagnostics' },
+    { '<leader>tIF', group = 'Diagflow' },
+    { '<leader>tIFe', diagflow_action('enable'), desc = 'Enable floating diagnostics' },
+    { '<leader>tIFd', diagflow_action('disable'), desc = 'Disable floating diagnostics' },
+    { '<leader>tIFt', diagflow_action('toggle'), desc = 'Toggle floating diagnostics' },
+    { '<leader>tIFc', diagflow_action('set_scope', 'cursor'), desc = 'Cursor scope' },
+    { '<leader>tIFl', diagflow_action('set_scope', 'line'), desc = 'Line scope' },
+    { '<leader>tIFs', diagflow_action('toggle_scope'), desc = 'Toggle scope' },
   })
   registered = true
 end
