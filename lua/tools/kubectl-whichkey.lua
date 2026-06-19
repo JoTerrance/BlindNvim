486621c3451d981be4835ea626414373b0501015
JoTerrance <gorka.zgz@gmail.com>
2026-06-19 12:28:23 +0200

    refactor: unify kubectl which-key bindings
    Move the kubectl keymap surface into lua/tools/kubectl-whichkey.lua\nand keep tK as the single entry point.\n\nRestore the pods, services, deployments, and namespaces shortcuts\nthere so the global which-key config no longer carries duplicate kubectl\nentries.

--- lua/tools/kubectl-whichkey.lua
+++ lua/tools/kubectl-whichkey.lua
@@ -9,15 +9,18 @@ local function register_which_key(desc_prefix)
   end
 
   wk.add({
-    { '<leader>uK', group = desc_prefix },
-    { '<leader>uKt', function() require('kubectl').toggle({ tab = false }) end, desc = desc_prefix .. ' toggle' },
-    { '<leader>uKT', function() require('kubectl').toggle({ tab = true }) end, desc = desc_prefix .. ' toggle tab' },
-    { '<leader>uKm', '<cmd>Kubectl<cr>', desc = desc_prefix .. ' main menu' },
-    { '<leader>uKv', '<cmd>Kubectl view<cr>', desc = desc_prefix .. ' view resource' },
-    { '<leader>uKx', '<cmd>Kubectx<cr>', desc = desc_prefix .. ' context' },
-    { '<leader>uKn', '<cmd>Kubens<cr>', desc = desc_prefix .. ' namespace' },
-    { '<leader>uKd', '<cmd>Kubectl diff<cr>', desc = desc_prefix .. ' diff' },
-    { '<leader>uKo', '<cmd>Kubectl top<cr>', desc = desc_prefix .. ' top' },
+    { '<leader>tK', group = desc_prefix },
+    { '<leader>tKt', function() require('kubectl').toggle({ tab = false }) end, desc = desc_prefix .. ' toggle' },
+    { '<leader>tKT', function() require('kubectl').toggle({ tab = true }) end, desc = desc_prefix .. ' toggle tab' },
+    { '<leader>tKm', '<cmd>Kubectl<cr>', desc = desc_prefix .. ' main menu' },
+    { '<leader>tKp', '<cmd>Kubectl pods<cr>', desc = desc_prefix .. ' pods' },
+    { '<leader>tKs', '<cmd>Kubectl services<cr>', desc = desc_prefix .. ' services' },
+    { '<leader>tKd', '<cmd>Kubectl diff<cr>', desc = desc_prefix .. ' diff' },
+    { '<leader>tKn', '<cmd>Kubens<cr>', desc = desc_prefix .. ' namespaces' },
+    { '<leader>tKD', '<cmd>Kubectl deployments<cr>', desc = desc_prefix .. ' deployments' },
+    { '<leader>tKv', '<cmd>Kubectl view<cr>', desc = desc_prefix .. ' view resource' },
+    { '<leader>tKx', '<cmd>Kubectx<cr>', desc = desc_prefix .. ' context' },
+    { '<leader>tKo', '<cmd>Kubectl top<cr>', desc = desc_prefix .. ' top' },
   })
 end
 
